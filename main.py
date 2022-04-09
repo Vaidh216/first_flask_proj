b0569697443c815cdf4c31dfd2f0e0aad31c3a10
Vaidh <vedprakashsingh216@gmail.com>
2022-04-09 23:55:03 +0530

    1

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "hellow this is ghurrahu singh <h1>Ghurrahu Singh<h1>"
+@app.route("/<name>/")
+def home(name):
+    return render_template("index.html")
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}"
- 
-@app.route("/contact")
-def contact():
-    return "<h2>This is the contact section<h2>";
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("contact"))
 
 if __name__ == "__main__":
     app.run()
